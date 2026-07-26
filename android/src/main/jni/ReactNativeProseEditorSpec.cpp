ff8223e2efa7524cec11bec058a15eb5dc396f2c
Jayden Smith <jayden@tas.dev>
2026-07-26 20:18:06 +1000

    fix(android): define component provider

---
+++ android/src/main/jni/ReactNativeProseEditorSpec.cpp
@@ -0,0 +1,13 @@
+#include "ReactNativeProseEditorSpec.h"
+
+namespace facebook {
+namespace react {
+
+std::shared_ptr<TurboModule> ReactNativeProseEditorSpec_ModuleProvider(
+    const std::string &moduleName,
+    const JavaTurboModule::InitParams &params) {
+  return nullptr;
+}
+
+} // namespace react
+} // namespace facebook
